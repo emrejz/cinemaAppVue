c8b64cccf5963a009ea22103d3bb874e542d59cb
emrejz <emre.kayacan@msn.com>
2019-10-10 23:45:50 +0300

    movieCard ok

--- src/pages/movie/movieCard.vue
+++ src/pages/movie/movieCard.vue
@@ -1,13 +1,39 @@
 <template>
-  <div>
-    Movie card
+  <div class="card">
+    <img :src="movie.Poster" class="card-img-top" :alt="movie.Title" />
+    <div class="card-body">
+      <h5 class="card-title">{{ movie.Title }}</h5>
+      <p class="card-text">Year: {{ movie.Year }}</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Type: {{ movie.Type }}</small>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
-    name: "MovieCard"
+    name: "MovieCard",
+    props: {
+      movie: {
+        type: Object,
+        required: true
+      }
+    }
   };
 </script>
 
-<style></style>
+<style>
+  .card {
+    margin: 10px;
+    width: 220px;
+    height: 400px;
+    text-align: center;
+  }
+  .card-img-top {
+    height: 220px;
+  }
+  .text-muted {
+    font-weight: bold;
+  }
+</style>
